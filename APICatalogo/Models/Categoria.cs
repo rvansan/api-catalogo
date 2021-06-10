3eba42ae1115969a6bd9e36e23a82714e9f0636a
Rafael Vansan <vansan.rafael@gmail.com>
2021-06-10 20:07:02 -0300

    models criados

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace APICatalogo.Controllers
+namespace APICatalogo.Models
 {
     public class Categoria
     {
