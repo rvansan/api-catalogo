a98cf547d78e4e7bcda19a043cf1df3b394b4bc9
Rafael Vansan <vansan.rafael@gmail.com>
2021-06-10 20:05:38 -0300

    models criados

---
+++ APICatalogo/Controllers/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Controllers
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ APICatalogo/Controllers/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Controllers
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImageUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
